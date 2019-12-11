0537be773dbfc39d81c7002baa743d27ebac4cc8
Wuzhengyu97 <49513890+Wuzhengyu97@users.noreply.github.com>
2019-12-11 20:10:43 +0800

    更新了文件上传的功能

--- src/main/java/cn/zhd/springboot/api/CloudApi.java
+++ src/main/java/cn/zhd/springboot/api/CloudApi.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,6 +28,51 @@ public class CloudApi {
 
     }
 
+    @RequestMapping("download")
+    public String getDownload(HttpServletRequest request, HttpServletResponse response)
+    {
+        String fileName = "abc.rar";// 文件名
+        if (fileName != null) {
+            //设置文件路径
+            File file = new File("E:/fileUpload/abc.rar");
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
 
     @RequestMapping("uploadFile")
     public void getUpload(@RequestParam("fileName") MultipartFile file)
